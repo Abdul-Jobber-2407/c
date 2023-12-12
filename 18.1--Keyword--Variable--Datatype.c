03e4b36a588cb0bda4bdea62d54295175f431c5d
Abdul-Jobber <153620521+Abdul-Jobber@users.noreply.github.com>
2023-12-12 20:18:15 +0600

    Add files via upload

---
+++ 18.1--Keyword--Variable--Datatype.c
@@ -0,0 +1,11 @@
+ /*An integer varibale num that will contain 10,
+ write a program that prints an intrger number */
+ #include<stdio.h>
+ int main()
+ {
+int num1=10,num2=20;
+ 
+printf("Numbers are = %d,%d",num1,num2);
+ 
+
+ }
