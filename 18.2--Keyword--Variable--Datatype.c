9f352d969d3479210ee320062535827a071a2fca
Abdul-Jobber <153620521+Abdul-Jobber@users.noreply.github.com>
2023-12-12 20:22:23 +0600

    Add files via upload

---
+++ 18.2--Keyword--Variable--Datatype.c
@@ -0,0 +1,17 @@
+ 
+
+    /*write a program that prints a floating,
+  double numberand character */
+
+  #include<stdio.h>
+  main()
+  {
+    float num1=10.5;
+    double num2=10.23467890;
+    char ch ='a';
+
+    printf("num1=%.1f",num1);
+    printf("\nnum2=%lf",num2);
+    printf("\nch=%c",ch);
+    
+  }     
