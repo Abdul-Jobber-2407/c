9020359d45c4c69124b5c1c7499ca47135887908
Abdul-Jobber <153620521+Abdul-Jobber@users.noreply.github.com>
2023-12-12 20:27:19 +0600

    Add files via upload

---
+++ 18.3--Keyword--Variable--Datatype.c
@@ -0,0 +1,13 @@
+ /*write a program that  takes an integer 
+  and print that number*/
+
+  #include<stdio.h>
+  int main()
+  {
+    int num;
+    printf ("please enter an integer=");
+    scanf("%d",&num);
+    printf ("You have pressed =%d\n",num);
+    
+    
+  } 
