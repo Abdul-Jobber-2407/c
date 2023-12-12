1aff007860bf7dbf2cfc4cf441599e2a427e5df3
Abdul-Jobber <153620521+Abdul-Jobber@users.noreply.github.com>
2023-12-13 02:02:35 +0600

    Add files via upload

---
+++ 23--Covert-value-into-Cherecter.c
@@ -0,0 +1,16 @@
+  // Convert ASCII Value into character
+  //Ascii value        Ascii cherecter
+  //65                  A
+  //97                  a
+  //76                  L
+  //108                 l
+
+ #include<stdio.h>
+ int main()
+{
+  int n;
+  printf("Enter any ASCII Value:");//97
+  scanf("%d",&n);
+  printf ("The Ascii character is :%c",n);//a
+  
+}
