85753598826cfb92d6e74fe3aaeb28dd0fb94715
Abdul-Jobber <153620521+Abdul-Jobber@users.noreply.github.com>
2023-12-13 02:07:51 +0600

    Add files via upload

---
+++ 24--Convert-Character-into-Ascii-value.c
@@ -0,0 +1,9 @@
+ // write a program that convert Character into Ascii value
+ #include<stdio.h>
+ int main()
+ {
+  char ch;
+  printf("Enter any character:");
+  scanf("%c",&ch);
+  printf("The ASCII value = %d\n",ch);
+ }
