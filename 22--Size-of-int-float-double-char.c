778cd03b318f44c16025f406cafa74754aaa8a93
Abdul-Jobber <153620521+Abdul-Jobber@users.noreply.github.com>
2023-12-13 01:46:23 +0600

    Add files via upload

---
+++ 22--Size-of-int-float-double-char.c
@@ -0,0 +1,14 @@
+  //write a size of int,float,double ,char;
+ 
+ #include<stdio.h>
+ int main()
+{
+ int i;
+ float f;
+ double d;
+ char c;
+ printf ("size of int =%d bytes\n",sizeof(i));
+ printf("size of float=%d bytes\n",sizeof(f));
+ printf("size of double=%d bytes\n",sizeof(d));
+ printf("size of char=%d bytes\n",sizeof(c));
+}
