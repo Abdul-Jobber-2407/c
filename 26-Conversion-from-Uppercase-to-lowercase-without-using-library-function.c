d9acafdf0728e0a4720e8ce7a9ebefcb449342c7
Abdul-Jobber <153620521+Abdul-Jobber@users.noreply.github.com>
2023-12-15 12:11:47 +0600

    Add files via upload

---
+++ 26-Conversion-from-Uppercase-to-lowercase-without-using-library-function.c
@@ -0,0 +1,9 @@
+ #include<stdio.h>
+ int main()
+ {
+  char upper;
+  printf("Enter any uppercase letter:");
+  scanf("%c",&upper);//A=65
+
+  printf("The lowercase letter:%c",upper+32);//a=97
+ }
---
+++ 27-Conversion-from-lowercase-to-Uppercase-using-library-function.c
@@ -0,0 +1,10 @@
+  #include<stdio.h>
+ int main()
+{
+  char lower, upper;
+  printf("Enter any lowercase letter:");
+  scanf("%c",&lower);
+
+  upper=(lower);
+  printf("uppercase letter =%c",upper);
+}
