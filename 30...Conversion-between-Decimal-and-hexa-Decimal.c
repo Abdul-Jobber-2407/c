43c628f68636ea4df036314b037082586095b32d
Abdul-Jobber <153620521+Abdul-Jobber@users.noreply.github.com>
2024-01-15 20:07:47 +0600

    Add files via upload

---
+++ 30...Conversion-between-Decimal-and-hexa-Decimal.c
@@ -0,0 +1,13 @@
+   #include<stdio.h>
+  int main()
+  {
+    int number;
+
+    printf("Decimal number = ");
+    scanf("%d",&number);
+
+    printf("Hexa-Decimal number=%X",number);
+  
+  
+    
+  }
