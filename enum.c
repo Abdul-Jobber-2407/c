41539e57eec909057d36842c5f6541b4db2947f5
Abdul-Jobber <153620521+Abdul-Jobber@users.noreply.github.com>
2024-01-17 16:52:45 +0600

    Add files via upload

--- enum.c
+++ enum.c
@@ -13,3 +13,18 @@
     int permission=EXECUTE|WRITE|READ;
     printf("%d",permission);
  }
+
+
+
+
+
+ #include<stdio.h>
+enum week{sun,mon,tue,wed,thu,fri,sat};
+
+int main()
+{
+ enum week today;
+ today=mon;
+ printf("Day %d",today+1);
+
+}
