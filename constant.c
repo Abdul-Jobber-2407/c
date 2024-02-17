c9363bf7e6908a0eae7411e3b63b055f4fbf7265
Abdul-Jobber <153620521+Abdul-Jobber@users.noreply.github.com>
2024-02-17 22:00:43 +0600

    Add files via upload

---
+++ constant.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    //defining integer constant using const keyword 
+    const int my_age = 21;
+
+    //defining float constant usting const keyword 
+    const float PI= 3.1416;
+
+    // defining character constant using const keyword 
+    const char char_const= 'A';
+
+    printf("Printing value of Integer Constant:%d\n",my_age);
+    printf("Printing value of Character constant : %c\n",char_const);
+    printf("Printing value of Float Constant:%f",PI);
+    return 0;
+
+}
