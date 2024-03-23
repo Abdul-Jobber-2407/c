34010cade3c43a7c66ced942fe124db92cfd39f0
Abdul-Jobber <153620521+Abdul-Jobber@users.noreply.github.com>
2024-03-23 12:10:00 +0600

    Add files via upload

---
+++ Write-a-C-program-to-reverse-the-elements of-an-array.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+
+{
+    int a [9]={34, 56, 54, 32, 67, 89, 90, 32, 21};
+    int i;
+
+    //orginal order
+    for (i=0; i<9; i++)
+    {
+        printf("%d",a[i]);
+    }
+    printf("\n");
+    // Reverse order
+    for (i=8;i>0;i--)
+    {
+        printf("%d",a[i]);
+    }
+    return 0;
+}
