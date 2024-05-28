98358fb2d170c4eeeef8608ab992104cb332dbe9
Abdul-Jobber <153620521+Abdul-Jobber@users.noreply.github.com>
2024-05-28 23:27:53 +0600

    Add files via upload

---
+++ Display-all-Elements-of-an-array-and-also-Display-sum-of-array.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+
+{
+    int a[5], i, sum=0;
+    printf("Enter array Elements: ");
+    for (i=0;i<5;i++)
+    {
+    scanf("%d",&a[i]);
+    }
+    printf("Sum of array Elements: ");
+    for (i=0;i<5; i++)
+    {
+        sum=sum+a[i];
+    }
+    printf("%d",sum);
+    return 0 ;
+
+}
+ 
